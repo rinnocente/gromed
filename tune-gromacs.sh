bd82a9fa38dd8d0920317f06c1aa11fa1a9c1369
rinnocente <rinnocente@users.noreply.github.com>
2017-05-11 13:20:40 +0200

    Update tune-gromacs.sh

--- tune-gromacs.sh
+++ tune-gromacs.sh
@@ -5,6 +5,7 @@
 #
 declare -a intel_cpu_flags_list=(mmx sse sse2 sse3 ssse3 atom_ssse3 sse4.1 sse4.2 atom_sse4.2 avx avx2 avx.512)
 #
+#
 tmpfile=`mktemp `
 for i in "${intel_cpu_flags_list[@]}"
 do
@@ -15,7 +16,8 @@ rm $tmpfile
 # 
 # SIMD compile flags supported by gromacs
 #
-declare -a  gromacs_flags_list=(None None SSE2 SSE2 SSE2 SSE2 SSE4.1 SSE4.1 SSE4.1 AVX_256 AVX2_256 AVX_512 AVX_512_KNL MIC Reference)
+# gromacs-5.1.4 gets crazy with AVX_512 flag, removed
+declare -a  gromacs_flags_list=(None None SSE2 SSE2 SSE2 SSE2 SSE4.1 SSE4.1 SSE4.1 AVX_256 AVX2_256 AVX_512)
 array_len=${#intel_cpu_flags_list[@]}
 for (( i=1; i<${array_len}+1; i++ ));
 do
@@ -23,6 +25,14 @@ do
 	        compile_flag="${gromacs_flags_list[$i-1]}"
 	fi
 done
+if [ ! -d build-"$compile_flag" ]
+then
+     GR_CORES=`cat /proc/cpuinfo |grep 'cpu cores'|uniq|sed -e 's/.*://'`
+     mkdir -p build-"$compile_flag" ; \
+     (cd build-"$compile_flag"; cmake .. \
+	 -DGMX_SIMD="$compile_flag" -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx  \
+	 -DGMX_THREAD_MPI:BOOL=OFF -DGMX_MPI:BOOL=ON ; make -j $((2*GR_CORES)); \
+     (cd build-"$compile_flag" ; make install)) \
+fi
 cd build-"$compile_flag"
 sudo make install
-
